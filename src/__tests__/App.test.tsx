ef11f2287563b6897e3c8402abd678881107a1e2
rubenszinho <samuelrubenscontato@gmail.com>
2024-12-03 20:49:19 -0300

    feat: add Vitest support and ESLint configuration for testing - Added `eslint-plugin-vitest` and `eslint-plugin-vitest-globals`. - Updated `eslint.config.js` with testing-related configurations. - Installed `@testing-library/react` and `@testing-library/jest-dom`. - Added npm scripts for testing (`test`) and coverage reporting (`coverage`).

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import App from '../App';
+import '@testing-library/jest-dom/';
+import { render, screen } from '@testing-library/react';
+
+test('renders the Vite + React heading', () => {
+  render(<App />);
+  const heading = screen.getByText(/Vite \+ React/i);
+  expect(heading).toBeInTheDocument();
+});
