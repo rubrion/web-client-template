f3d84d8462ef09211b676343e24370df31037e19
rubenszinho <samuelrubenscontato@gmail.com>
2024-12-06 17:10:31 -0300

    chore: add ESLint plugin for organizing imports
    - Added `eslint-plugin-simple-import-sort` to dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import vitest from 'eslint-plugin-vitest';
+import globals from 'globals';
 
 export default [
   {
@@ -32,6 +33,7 @@ export default [
       'react-refresh': reactRefresh,
       prettier,
       '@typescript-eslint': tsPlugin,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -45,6 +47,8 @@ export default [
       '@typescript-eslint/no-unused-vars': 'warn',
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
     settings: {
       react: {
